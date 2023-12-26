7a63ea29f2046ce71de1d092d71de1172782d563
LunqiZhao <1085789239@qq.com>
2023-12-26 18:04:47 +0800

    optimize vbf struct memory allocation

--- src/bpf_kern/member_vbf.c
+++ src/bpf_kern/member_vbf.c
@@ -14,6 +14,8 @@ char _license[] SEC("license") = "GPL";
 #define HASH_SEED_2 0xaaaabbbb
 #define MEMBER_NO_MATCH 0
 
+#define EINVAL 1
+#define ENOSPC 2
 /* datastruct params */
 #define NUM_KEYS 100
 #define NUM_SET 8
@@ -44,7 +46,7 @@ ctz32(__u32 v)
 }
 
 /* function macro */
-#define MEMBER_LOOKUP_VBF(key, key_len, set_id)		\
+#define MEMBER_LOOKUP_VBF(table, key, key_len, set_id)		\
 	j, h1, h2;																			\
 	bit_loc;																				\
 	mask = ~0;																			\
@@ -52,7 +54,7 @@ ctz32(__u32 v)
 	h2 = jhash(&h1, sizeof(h1), HASH_SEED_2);				\
 	for (j = 0; j < NUM_HASHES; j++) {							\
 		bit_loc = (h1 + j * h2) & BIT_MASK;						\
-		mask &= test_bit(bit_loc);										\
+		mask &= test_bit(table, bit_loc);										\
 	}																								\
 	if (mask) {																			\
 		set_id = ctz32(mask) + 1;											\
@@ -62,7 +64,7 @@ ctz32(__u32 v)
 		log_info("key %d not founded, set_id: %d\n", key, set_id);							\
 	}																								\
 
-#define MEMBER_ADD_VBF(key, key_len, set_id)			\
+#define MEMBER_ADD_VBF(table, key, key_len, set_id)			\
 	i, h1, h2;																			\
 	bit_loc;																				\
 	if (set_id > NUM_SET || set_id == MEMBER_NO_MATCH)	\
@@ -71,65 +73,49 @@ ctz32(__u32 v)
 	h2 = jhash(&h1, sizeof(h1), HASH_SEED_2);				\
 	for (i = 0; i < NUM_HASHES; i++) {							\
 		bit_loc = (h1 + i * h2) & BIT_MASK;						\
-		set_bit(bit_loc, set_id);											\
+		set_bit(table, bit_loc, set_id);											\
 	}																								\
 
 /* core malloc area */
+struct vbf_memory {
+	__u32 table[MAX_ENTRY]
+};
 struct {
 	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
 	__type(key, int);
-	__type(value, __u32);  
-	__uint(max_entries, MAX_ENTRY);
+	__type(value, struct vbf_memory);  
+	__uint(max_entries, 1);
 } map SEC(".maps");
 
 /* vBF helper implementation */
-inline __u32
-test_bit(__u32 bit_loc)
+static __always_inline __u32
+test_bit(__u32 *table, __u32 bit_loc)
 {
-	__u32 n = NUM_SET;
-	/*
-	 * a is how many bits in one BF are represented by one 32bit
-	 * variable.
-	 */
-	__u32 a = 32 >> MUL_SHIFT;
-	/*
-	 * x>>b is the divide, x & (a-1) is the mod, & (1<<n-1) to mask out bits
-	 * we do not need
-	 */
-	__u32 key = bit_loc >> DIV_SHIFT;
-
-	const __u32 *value = bpf_map_lookup_elem(&map, &key);
-	// log_info("testbit() lookup key: %x\n", key);
-	if (value == NULL) {
-		// log_error("bpf_map_lookup_elem failed at line: %d\n", __LINE__);
-		return -1;
+	if ((bit_loc >> DIV_SHIFT) >= MAX_ENTRY) {
+		/* when lookup failed, will heat this */
+		log_debug("bit_loc error");
+		return -EINVAL;
 	}
-	__u32 value_new = *value;
-	// log_info("test_bit() value_new: %u\n", value_new);
-	return (value_new >> (((bit_loc & (a - 1)) << MUL_SHIFT))) & ((1ULL << n) - 1);
+	__u32 a = 32 >> MUL_SHIFT;
+	return (table[bit_loc >> DIV_SHIFT] >>
+			((bit_loc & (a - 1)) << MUL_SHIFT)) & ((1ULL << NUM_SET) - 1);
 }
 
-inline void
-set_bit(__u32 bit_loc, __s32 set)
+static __always_inline void
+set_bit(__u32 *table, __u32 bit_loc, __s32 set)
 {
-	__u32 a = 32 >> MUL_SHIFT;
-	__u32 key = 0;
-	key = bit_loc >> DIV_SHIFT;
-	__u32 *value = bpf_map_lookup_elem(&map, &key);
-	__u32 value_new;
-	if (value == NULL) {
-		// log_error("bpf_map_lookup_elem failed at line: %d\n", __LINE__);
-		value_new = 0;
-	} else {
-		value_new = *value;
+	if ((bit_loc >> DIV_SHIFT) >= MAX_ENTRY) {
+		log_error("bit_loc error");
+		return;
 	}
-	value_new |= 1UL << (((bit_loc & (a - 1)) << MUL_SHIFT) + set - 1);
-	bpf_map_update_elem(&map, &key, &value_new, BPF_ANY);
+	__u32 a = 32 >> MUL_SHIFT;
+	table[bit_loc >> DIV_SHIFT] |=
+			1UL << (((bit_loc & (a - 1)) << MUL_SHIFT) + set - 1);
 }
 
 /* vBF API implementation */
 static __always_inline int
-member_lookup_vbf(__u32 *key, __u32 key_len, __s32 *set_id)
+member_lookup_vbf(__u32 *table, __u32 *key, __u32 key_len, __s32 *set_id)
 {
 	__u32 j;
 	if (key == NULL || set_id == NULL) {
@@ -142,7 +128,7 @@ member_lookup_vbf(__u32 *key, __u32 key_len, __s32 *set_id)
 
 	for (j = 0; j < NUM_HASHES; j++) {
 		bit_loc = (h1 + j * h2) & BIT_MASK;
-		mask &= test_bit(bit_loc);
+		mask &= test_bit(table, bit_loc);
 	}
 
 	if (mask) {
@@ -157,7 +143,7 @@ member_lookup_vbf(__u32 *key, __u32 key_len, __s32 *set_id)
 }
 
 static __always_inline int
-member_add_vbf(__u32 *key, __u32 key_len, __s32 set_id)
+member_add_vbf(__u32 *table, __u32 *key, __u32 key_len, __s32 set_id)
 {
 	__u32 i, h1, h2;
 	__u32 bit_loc;
@@ -170,7 +156,7 @@ member_add_vbf(__u32 *key, __u32 key_len, __s32 set_id)
 
 	for (i = 0; i < NUM_HASHES; i++) {
 		bit_loc = (h1 + i * h2) & BIT_MASK;
-		set_bit(bit_loc, set_id);
+		set_bit(table, bit_loc, set_id);
 	}
 	return 0;
 }
@@ -185,13 +171,21 @@ int test_vbf(struct xdp_md *ctx) {
 	__u32 bit_loc;
 	__u32 mask;
 
+	int index = 0;
+	struct vbf_memory *__vbf = bpf_map_lookup_elem(&map, &index);
+	if (__vbf == NULL) {
+		log_error("memory initialization error");
+		return XDP_PASS;
+	}
+	__u32 *table = __vbf->table;
+
 	for (int k = 10; k < 20; k += 2) {
-		MEMBER_ADD_VBF(k, key_len, set_id)
-		// member_add_vbf(&i, key_len, set_id);
+		MEMBER_ADD_VBF(table, k, key_len, set_id)
+		// member_add_vbf(table, &i, key_len, set_id);
 	}
 	for (int v = 10; v < 20; v++) {
-		MEMBER_LOOKUP_VBF(v, key_len, set_id)
-		// member_lookup_vbf(&v, key_len, &set_id);
+		MEMBER_LOOKUP_VBF(table, v, key_len, set_id)
+		// member_lookup_vbf(table, &v, key_len, &set_id);
 	}
 	return XDP_PASS;
 }
