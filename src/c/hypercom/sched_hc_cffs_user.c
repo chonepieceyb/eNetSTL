3e22e1118f2dab3b3d200d2d7065dfdd0a2db0a4
chonepieceyb <1759315491@qq.com>
2023-12-27 16:32:20 +0800

    add sched cffs hp

---
+++ src/c/hypercom/sched_hc_cffs_user.c
@@ -0,0 +1,49 @@
+#include "../common.h" 
+#include <stdio.h>
+#include <assert.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "../bpf_skel/sched_hc_cFFS_PIQ.skel.h"
+#include <net/if.h>
+#include <linux/if_link.h>
+
+#include "../test_helpers.h"
+
+#define IF_NAME "ens4np0"
+
+int main() {
+        struct sched_hc_cFFS_PIQ * skel = NULL;
+        struct bpf_program *prog;
+        int fd, ifindex, res;
+        res = 0;
+        ifindex = if_nametoindex(IF_NAME);
+        if (ifindex == 0) {
+                printf("failed to get ifindex %s\n", strerror(errno));
+                return -1;
+        }
+        skel = sched_hc_cFFS_PIQ__open();
+        if (skel == NULL) {
+                printf("faild to open and load hw_demo\n");
+                return -1; 
+        }
+        prog = skel->progs.xdp_main;
+        res = sched_hc_cFFS_PIQ__load(skel);
+        if (res) {
+                printf("faild to load, res %d %s\n", res, strerror(errno));
+                goto clean;
+        }
+                
+        fd = bpf_program__fd(prog);
+        
+        //res = bpf_xdp_attach(index, fd, XDP_FLAGS_HW_MODE, NULL);
+        res = bpf_xdp_attach(ifindex, fd, XDP_FLAGS_DRV_MODE, NULL);
+        if (res < 0) {
+                printf("failed to offload prog %d to nic %d, res: %d , %s\n", fd, ifindex, res, strerror(errno));
+                goto clean;
+        }
+clean:;
+        sched_hc_cFFS_PIQ__destroy(skel);
+        return res;
+}
