7d7404025aa855f4df772a2524cca34ad3116e34
Yang Hanlin <mattoncis@hotmail.com>
2024-01-27 16:55:34 +0800

    update gen_geo to use MAX_GEOSAMPLING_SIZE 1024

--- scripts/gen_geo_sampling_pool_h.sh
+++ scripts/gen_geo_sampling_pool_h.sh
@@ -12,6 +12,6 @@ python3 "$PROJECT_DIR/src/python/gen_geo.py" \
     -o "$PROJECT_DIR/LKM/bpf_random_base_alg/geo_sampling_pool.h" \
     -o "$PROJECT_DIR/LKM/sk_nitro/geo_sampling_pool.h" \
     --cpus 40 \
-    --max-geosampling-size 512 \
+    --max-geosampling-size 1024 \
     --geo-cnt-cap 2147483647 \
     --geo-cnt-type uint32_t
