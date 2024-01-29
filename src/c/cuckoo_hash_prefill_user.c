41064fe99d78762bd8dd791374b4e7fbe46c3b7f
Yang Hanlin <mattoncis@hotmail.com>
2024-01-30 02:38:22 +0800

    fix prefill for cuckoo hash eBPF & HyperCom (eNetSTL)

--- src/c/cuckoo_hash_prefill_user.c
+++ src/c/cuckoo_hash_prefill_user.c
@@ -5,36 +5,71 @@
 
 #include "bpf_skel/cuckoo_hash_prefill.skel.h"
 
-#define set_test_cpu(skel)       \
-	({                       \
-		topts.cpu = cpu; \
-		0;               \
+#define __USE_GNU
+#include <sched.h>
+
+#define set_test_cpu(skel)                                       \
+	({                                                       \
+		topts.cpu = cpu; /* FIXME: this does not work */ \
+		0;                                               \
 	})
 
-void run_prefill_with_cpu(uint32_t cpu)
+void __run_prefill_with_cpu(uint32_t cpu)
 {
 	BPF_PROG_TEST_RUNNER_WITH_CALLBACK("cuckoo_hash prefill",
 					   cuckoo_hash_prefill, pkt_v4, prefill,
 					   1, set_test_cpu, 0);
 }
 
+int run_prefill_with_cpu(uint32_t cpu)
+{
+	__run_prefill_with_cpu(cpu);
+	return 0;
+}
+
+void __run_prefill_with_cpu2(void)
+{
+	BPF_PROG_TEST_RUNNER("cuckoo_hash prefill", cuckoo_hash_prefill, pkt_v4,
+			     prefill, 1, 0);
+}
+
+int run_prefill_with_cpu2(uint32_t cpu)
+{
+	int res = 0;
+	cpu_set_t cpuset;
+
+	CPU_ZERO(&cpuset);
+	CPU_SET(cpu, &cpuset);
+	if ((res = sched_setaffinity(0, sizeof(cpuset), &cpuset)) != 0) {
+		printf("Failed to set affinity: %d\n", res);
+		return res;
+	}
+
+	__run_prefill_with_cpu2();
+
+	return 0;
+}
+
 int main()
 {
-	int ncpus;
+	int ncpus, res, ret = 0;
 
 	if ((ncpus = libbpf_num_possible_cpus()) < 0) {
-		printf("Failed to get number of cpus\n");
+		printf("Failed to get number of CPUs\n");
 		return 1;
 	} else {
-		printf("Got %d cpus\n", ncpus);
+		printf("Got %d CPUs\n", ncpus);
 	}
 
 	for (int i = 0; i < ncpus; i++) {
 		printf("Prefilling with CPU %d\n", i);
-		run_prefill_with_cpu(i);
+		if ((res = run_prefill_with_cpu2(i)) != 0) {
+			printf("Failed to prefill with CPU %d: %d\n", i, res);
+			ret++;
+		}
 	}
 
 	printf("Prefill done\n");
 
-	return 0;
+	return ret;
 }
