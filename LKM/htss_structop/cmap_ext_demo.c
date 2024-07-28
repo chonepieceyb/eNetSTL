503e1911ba4b01b501b4a6fe98ac9651c402b09b
LunqiZhao <1085789239@qq.com>
2024-07-28 22:41:56 +0800

    feat: add htss struct op version

---
+++ LKM/htss_structop/cmap_ext_demo.c
@@ -0,0 +1,289 @@
+#include "asm-generic/errno-base.h"
+#include "linux/stddef.h"
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/bpf.h>
+#include <linux/bpf_custom_map.h>
+#include <linux/string.h>
+#include <linux/math.h>
+
+/* include SIMD header */
+#if defined (USE_SIMD) || defined (USE_SIMD_HASH)
+#define _MM_MALLOC_H_INCLUDED
+#include <immintrin.h>
+#endif
+
+#ifdef USE_SIMD_HASH
+#include "crc32hash.h"
+#define HASH_FUNC rte_hash_crc
+#else
+#include "fasthash.h"
+#define HASH_FUNC fasthash32
+#endif
+
+/* static vars */
+#define MAX_ENTRY 2048
+#define HASH_SEED_1 0xdeadbeef
+#define HASH_SEED_2 0xaaaabbbb
+#define MEMBER_NO_MATCH 0
+#define MEMBER_MAX_PUSHES 8
+
+/* datastruct params */
+#define NUM_ENTRIES 1024
+#define NUM_BUCKETS 128
+#define SIZE_BUCKET_T 32
+#define BUCKET_MASK 127
+#define MEMBER_BUCKET_ENTRIES 8
+
+// extern int bpf_register_custom_map(struct bpf_custom_map_ops *cmap);
+// extern void bpf_unregister_custom_map(struct bpf_custom_map_ops *cmap);
+extern int bpf_register_static_cmap(struct bpf_map_ops *map, struct module *onwer);
+extern void bpf_unregister_static_cmap(struct module *onwer);
+extern void bpf_map_area_free(void *area);
+extern void *bpf_map_area_alloc(u64 size, int numa_node);
+
+/* bitwise operation */
+static inline __u32
+ctz32(__u32 v)
+{
+	return (unsigned int)__builtin_ctz(v);
+}
+
+typedef __u16 sig_t;
+typedef __u16 set_t;
+struct member_ht_bucket {
+	sig_t sigs[MEMBER_BUCKET_ENTRIES];
+	set_t sets[MEMBER_BUCKET_ENTRIES];
+};
+struct htss_memory {
+	struct member_ht_bucket buckets[NUM_BUCKETS];
+	rwlock_t rw_lock;
+};
+
+struct mod_struct_ops_ctx {
+	struct htss_memory *__htss_memory;
+};
+
+struct mod_struct_ops_demo {
+	int (*hello_world)(struct mod_struct_ops_ctx *ctx); 
+	struct module *owner;
+};
+
+struct static_htss_map_structop {
+	struct bpf_map map;
+	struct mod_struct_ops_demo *ext_ops;
+	struct mod_struct_ops_ctx ctx;
+};
+
+// static DEFINE_SPINLOCK(ops_mutex);
+// static struct mod_struct_ops_demo __rcu *ext_ops = NULL;
+
+struct mod_struct_ops_demo *bpf_ops = NULL;
+static DEFINE_SPINLOCK(static_ops_lock);
+
+
+static int htss_loop_up_eBPF(struct mod_struct_ops_ctx *ctx) {
+	ctx->__htss_memory = NULL;
+	return 0;
+}
+
+DEFINE_STATIC_CALL_RET0(__htss_loop_up_eBPF, htss_loop_up_eBPF);
+
+
+static int static_htss_loop_up_eBPF(struct mod_struct_ops_ctx *ctx) {
+	return static_call(__htss_loop_up_eBPF)(ctx);
+}
+
+void set_default_static_funcs(void) 
+{
+	static_call_update(__htss_loop_up_eBPF, htss_loop_up_eBPF);
+}
+
+int reg_cmap_ext_demo_ops(struct mod_struct_ops_demo *new_ext_ops) 
+{
+	int res = 0;
+	spin_lock(&static_ops_lock);
+	if (bpf_ops != NULL) {
+		res = -EEXIST;
+		goto error;
+	}
+	if (!bpf_try_module_get(new_ext_ops, new_ext_ops->owner)) {
+		pr_err("failed to get bpf module");
+		goto error;
+	}
+	bpf_ops = new_ext_ops;
+	/* we have get bpf module now*/
+	if (new_ext_ops->hello_world != NULL) {
+		static_call_update(__htss_loop_up_eBPF, new_ext_ops->hello_world);
+		pr_debug("mod_struct_ops_demo update hello world");
+	} else {
+		pr_err("mod_struct_ops_demo reg not have hello world");
+		res = -EINVAL;
+		goto error_set_default;
+	}
+	spin_unlock(&static_ops_lock);
+	return 0;
+
+error_set_default:;
+	bpf_module_put(new_ext_ops, new_ext_ops->owner);
+	bpf_ops = NULL;
+	set_default_static_funcs();
+error:
+	spin_unlock(&static_ops_lock);
+	return res; 
+}
+EXPORT_SYMBOL(reg_cmap_ext_demo_ops);
+
+void unreg_cmap_ext_demo_ops(struct mod_struct_ops_demo *ops)
+{
+	spin_lock(&static_ops_lock);
+	if (bpf_ops != NULL) {
+		bpf_module_put(bpf_ops, bpf_ops->owner);
+		bpf_ops = NULL;
+		set_default_static_funcs();
+	}
+	spin_unlock(&static_ops_lock);
+}
+EXPORT_SYMBOL(unreg_cmap_ext_demo_ops);
+
+int map_ext_alloc_check(union bpf_attr *attr) 
+{
+	return attr->value_size == sizeof(struct mod_struct_ops_ctx);
+}
+
+static struct bpf_map *map_ext_alloc(union bpf_attr *attr)
+{
+	struct static_htss_map_structop *htss_map;
+	htss_map = bpf_map_area_alloc(sizeof(*htss_map), -1);
+	if (!htss_map)
+		return ERR_PTR(-ENOMEM);
+	
+	htss_map->ctx.__htss_memory = kmalloc(attr->key_size * NUM_BUCKETS * MEMBER_BUCKET_ENTRIES, GFP_ATOMIC);
+	if (htss_map->ctx.__htss_memory == NULL) {
+		return ERR_PTR(-ENOMEM);
+	}
+	struct htss_memory* __htss = htss_map->ctx.__htss_memory;
+	memset(__htss, 0, sizeof(struct htss_memory));
+	rwlock_init (&__htss->rw_lock);
+	return (struct bpf_map*)htss_map;
+}
+
+static void map_ext_free(struct bpf_map *map) {
+	struct static_htss_map_structop *htss_map;
+	if (map == NULL) {
+		return;
+	}
+	htss_map = container_of(map, struct static_htss_map_structop, map);
+
+	kfree(htss_map->ctx.__htss_memory);
+	bpf_map_area_free(htss_map);
+	return;
+}
+
+static void* map_ext_lookup_elem(struct bpf_map *map, void *key) 
+{
+	struct static_htss_map_structop *htss_map = container_of(map, struct static_htss_map_structop, map);
+	struct htss_memory *__htss = htss_map->ctx.__htss_memory;
+
+	__u32 prim_bucket, sec_bucket;
+	sig_t tmp_sig;
+	set_t *search_res;
+
+	// add read lock
+	read_lock(&__htss->rw_lock);
+	struct member_ht_bucket *buckets = __htss->buckets;
+
+	//todo: add eBPF version impl here, and delete below code segment
+
+	// get_buckets_index(key, map->key_size, &prim_bucket, &sec_bucket, &tmp_sig);
+	// search_res = SERCH_BUCKET_FUNC(prim_bucket, tmp_sig, buckets);
+	// if (search_res == NULL) {
+	// 	search_res = SERCH_BUCKET_FUNC(sec_bucket, tmp_sig, buckets);
+	// }
+
+	// implement lookup in this eBPF function
+	static_htss_loop_up_eBPF(&htss_map->ctx);
+	read_unlock(&__htss->rw_lock);
+	return search_res;
+}
+
+static long map_ext_update_elem(struct bpf_map *map, void *key, void *value, u64 flags) 
+{
+	struct static_htss_map_structop *htss_map = container_of(map, struct static_htss_map_structop, map);
+	struct htss_memory *__htss = htss_map->ctx.__htss_memory;
+	
+	long ret;
+	unsigned int nr_pushes = 0;
+	__u32 prim_bucket = 0;
+	__u32 sec_bucket = 0;
+	sig_t tmp_sig = 0;
+	set_t flag_mask = 1U << (sizeof(set_t) * 8 - 1);
+	__u32 set_id = *(__u32 *)value;
+
+	if (set_id == MEMBER_NO_MATCH || (set_id & flag_mask) != 0) {
+		return -1;
+	}
+	// add writer lock
+	write_lock(&__htss->rw_lock);
+	struct member_ht_bucket *buckets = __htss->buckets;
+
+	//todo: add eBPF version impl here, and delete below code segment
+
+	// get_buckets_index(key, map->key_size, &prim_bucket, &sec_bucket, &tmp_sig);
+	// ret = try_insert(buckets, prim_bucket, sec_bucket, tmp_sig, set_id);
+	// if (ret != -1)
+	// 	goto unlock;
+
+	// /* Random pick prim or sec for recursive displacement */
+	// __u32 select_bucket = (tmp_sig && 1U) ? prim_bucket : sec_bucket;
+
+	// ret = make_space_bucket(buckets, select_bucket, &nr_pushes);
+	// if (ret >= 0) {
+	// 	buckets[select_bucket].sigs[ret] = tmp_sig;
+	// 	buckets[select_bucket].sets[ret] = set_id;
+	// 	ret = 1;
+	// 	goto unlock;
+	// }
+	static_htss_loop_up_eBPF(&htss_map->ctx);
+unlock:
+	write_unlock(&__htss->rw_lock);
+	return ret;
+}
+
+static long map_ext_delete_elem(struct bpf_map *map, void *key) {
+	return 0;
+}
+
+static u64 map_ext_mem_usage(const struct bpf_map *map) 
+{
+	return sizeof(struct htss_memory) * num_possible_cpus();
+}
+
+static struct bpf_map_ops map_ext_ops = {
+	.map_alloc = map_ext_alloc,
+	.map_free = map_ext_free,
+	.map_lookup_elem = map_ext_lookup_elem,
+	.map_update_elem = map_ext_update_elem,
+	.map_delete_elem = map_ext_delete_elem,
+	.map_mem_usage = map_ext_mem_usage
+};
+
+static int __init map_ext_cmaps_init(void) {
+	pr_info("register static map_ext_cmaps");
+	return bpf_register_static_cmap(&map_ext_ops, THIS_MODULE);
+}
+
+static void __exit map_ext_cmaps_exit(void) {
+	pr_info("unregister static map_ext_cmaps");
+	bpf_unregister_static_cmap(THIS_MODULE);
+}
+
+/* Register module functions */
+module_init(map_ext_cmaps_init);
+module_exit(map_ext_cmaps_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("chonepieceyb");
+MODULE_DESCRIPTION("A simple CMAP with bpf module struct ops extension demo.");
+MODULE_VERSION("0.01");
