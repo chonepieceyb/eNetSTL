1671382a0f8a1a91dbd284d6e8e9a4d136fe6829
Yang Hanlin <mattoncis@hotmail.com>
2024-07-29 18:18:33 +0800

    (temporarily) update kernel testbed scripts

--- scripts/debug_kernel_testbed.sh
+++ scripts/debug_kernel_testbed.sh
@@ -7,7 +7,7 @@ function echo_help() {
     echo "add -S to hold on when enabling gdb"
     echo "run ./create_LKM_link.sh"
     echo "gdb vmlinux"
-    echo "target remote :1234"
+    echo "target remote :3234"
     echo "lx-symbols to reload vmlinux and LKMs symbol"
 }
 
@@ -15,9 +15,9 @@ echo_help
 
 ${SCRIPT_DIR}/copy_LKM.sh
 
-session_name="kernel-testbed"
+session_name="kernel-testbed-yhl"
 
 sudo qemu-system-x86_64 --enable-kvm -m 4G -smp 4 -cpu host -boot c \
     -hda ${PROJECT_DIR}testing/kernel-testbed.img -device virtio-net-pci,netdev=net0    \
-    -netdev user,hostfwd=tcp::2222-:22,id=net0 -nographic -append " root=/dev/sda2 console=ttyS0 nokaslr" \
-    -kernel ${PROJECT_DIR}${LINUX}/arch/x86_64/boot/bzImage -s -S
+    -netdev user,hostfwd=tcp::4222-:22,id=net0 -nographic -append " root=/dev/sda2 console=ttyS0 nokaslr" \
+    -kernel ${PROJECT_DIR}${LINUX}/arch/x86_64/boot/bzImage -gdb tcp::3234
--- scripts/kill_kernel_gdb_session.sh
+++ scripts/kill_kernel_gdb_session.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-session_name="kernel-testbed"
+session_name="kernel-testbed-yhl"
 sudo tmux kill-session -t $session_name > /dev/null 2>&1
