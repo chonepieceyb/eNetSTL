5eafc68066a1f4d996ae29daa38da03f33385dab
Yang Hanlin <mattoncis@hotmail.com>
2024-07-30 12:13:13 +0800

    attach `hypercom_sk_cm2` and `hypercom_heavy_keeper` to real XDP hookpoints

--- src/c/hypercom/hypercom_sk_cm2_user.c
+++ src/c/hypercom/hypercom_sk_cm2_user.c
@@ -3,32 +3,24 @@
 #include "../bpf_skel/hypercom_sk_cm2_ext.skel.h"
 #include "../test_helpers.h"
 
+#define XDP_IF "ens4np0"
+
 struct hypercom_sk_cm2_ext *load_st_ops(void)
 {
 	BPF_MOD_LOAD_STRUCT_OPS(hypercom_sk_cm2_ext, hash_ops,
 				"hash_mod_struct_ops");
 }
 
-void test_run()
-{
-	BPF_PROG_TEST_RUNNER("test hash_mod_struct_ops", hypercom_sk_cm2_base,
-			     pkt_v4, xdp_main, 1, 1);
-}
-
-/* FIXME: Attach to real XDP hookpoint */
-void test()
-{
-	struct hypercom_sk_cm2_ext *skel;
-	skel = load_st_ops();
-	if (skel == NULL)
-		return;
-	test_run();
-	int key = 0;
-	bpf_map__delete_elem(skel->maps.hash_ops, &key, sizeof(key), 0);
-	hypercom_sk_cm2_ext__destroy(skel);
-}
-
 int main()
 {
-	test();
+	/* TODO: Find pinned map and delete previous ops */
+
+	struct hypercom_sk_cm2_ext *ext_skel = load_st_ops();
+	if (ext_skel == NULL) {
+		return 1;
+	}
+	hypercom_sk_cm2_ext__destroy(ext_skel);
+
+	BPF_XDP_SKEL_LOADER(hypercom_sk_cm2_base, XDP_IF, xdp_main,
+			    XDP_FLAGS_DRV_MODE)
 }
