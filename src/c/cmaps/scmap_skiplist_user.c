27be9d1acebaa3a419bf765f2c3dff4cbd8a7613
LunqiZhao <1085789239@qq.com>
2024-08-01 00:07:33 +0800

    feat: add skiplist exp scripts

--- src/c/cmaps/scmap_skiplist_user.c
+++ src/c/cmaps/scmap_skiplist_user.c
@@ -1,12 +1,23 @@
-#include "../common.h" 
+#include "../common.h"
 #include "../bpf_skel/scmap_skiplist.skel.h"
-#include "../test_helpers.h"
 
-void test() {
-        BPF_PROG_TEST_RUNNER("scmap_skiplist", scmap_skiplist, pkt_v4, xdp_test, 1, 0);
+// 修改这里控制变量
+#define KEY_RANGE 4096
+
+static int callback_load(struct scmap_skiplist *skel)
+{
+  struct bpf_map *skip_list = skel->maps.skip_list;
+  //初始化skip_list，填入KEY_RANGE个元素
+  for (u64 i = 0; i < KEY_RANGE; i++) {
+    int res = bpf_map__update_elem(skip_list, &i, sizeof(u64), &i,
+                 sizeof(u64), BPF_ANY);
+  }
+  printf("KEY_RANGE: %d, skip_list init finished.\n", KEY_RANGE);
+  return 0;
 }
-int main() {
-//        test();
-//        return 0;
-       BPF_XDP_SKEL_LOADER(scmap_skiplist, "ens4np0", pop_test, XDP_FLAGS_DRV_MODE)
+
+int main()
+{
+  BPF_XDP_SKEL_LOADER_WITH_CALLBACK(scmap_skiplist, "ens2f0", xdp_main,
+            callback_load, XDP_FLAGS_DRV_MODE)
 }
